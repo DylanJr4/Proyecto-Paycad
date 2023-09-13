729ea09b52b21e5768443d31e7e809892683a535
Dylan Rodriguez <dylanjrodriguez4@gmail.com>
2023-09-13 11:22:49 -0500

    estructura del proyecto

---
+++ app/src/main/java/com/example/paycad/controller/ActivityVerificationScreen.kt
@@ -0,0 +1,12 @@
+package com.example.paycad.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.paycad.R
+
+class ActivityVerificationScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification_screen)
+    }
+}
