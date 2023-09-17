9ec20cf9470a0cb9006c5ba0ff74fc077845f301
Dylan Rodriguez <dylanjrodriguez4@gmail.com>
2023-09-17 14:31:59 -0500

    realizacion del splash screen

--- app/src/main/java/com/example/paycad/controller/MainActivitySplashScreen.kt
+++ app/src/main/java/com/example/paycad/controller/MainActivitySplashScreen.kt
@@ -1,12 +1,40 @@
 package com.example.paycad.controller
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
 import com.example.paycad.R
 
 class MainActivitySplashScreen : AppCompatActivity() {
+    private lateinit var countdownTimer: CountDownTimer
+    private lateinit var textViewTimer: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_splash_screen)
+
+        textViewTimer = findViewById(R.id.segundos_screen)
+
+        // Inicializar el temporizador
+        countdownTimer = object : CountDownTimer(3000, 1000) {
+            @SuppressLint("StringFormatInvalid")
+            override fun onTick(millisUntilFinished: Long) {
+                // Actualizar el texto del temporizador
+                textViewTimer.text = getString(R.string.timer, millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                // Cambiar a la actividad principal
+                startActivity(Intent(this@MainActivitySplashScreen, ActivityAboutScreen::class.java))
+                finish()
+            }
+        }
+
+        // Iniciar el temporizador
+        countdownTimer.start()
     }
 }
+
