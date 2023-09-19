5c7d28d658dc2367936079e5204aea2c5bb01e04
Dylan Rodriguez <dylanjrodriguez4@gmail.com>
2023-09-19 13:35:48 -0500

    prueba de code nuevo

--- app/src/main/java/com/example/paycad/controller/ActivityAboutScreen.kt
+++ app/src/main/java/com/example/paycad/controller/ActivityAboutScreen.kt
@@ -1,13 +1,42 @@
 package com.example.paycad.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.paycad.R
 
 class ActivityAboutScreen : AppCompatActivity() {
+    private lateinit var btnNext: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_screen)
+
+        btnNext= findViewById(R.id.btnNext)
+        // Creamos una variable para almacenar el estado de la aplicación
+        val isAppInBackground = savedInstanceState?.getBoolean("isAppInBackground") ?: false
+
+        // Si la aplicación estaba en segundo plano, mostramos el splash screen
+        if (isAppInBackground) {
+            startActivity(Intent(this, MainActivitySplashScreen::class.java))
+            finish()
+        }
+
+        // Agregamos un evento al botón btnNext
+        btnNext.setOnClickListener {
+            // Iniciamos la nueva actividad
+            startActivity(Intent(this, ActivityGenerationCodeScreen::class.java))
+        }
     }
 
+    override fun onBackPressed() {
+        // Si el usuario solo presiona el botón de atrás, dejamos que la aplicación se cierre
+        if (supportFragmentManager.backStackEntryCount == 0) {
+            super.onBackPressed()
+        } else {
+            // Si el usuario presiona el botón de atrás desde un fragmento, navegamos a la pantalla anterior
+            supportFragmentManager.popBackStack()
+        }
+    }
 }
