5c421797a88f5ce6ebae9f41653b2e3a8fa742b5
Jesus Florez <jflorez7k@gmail.com>
2023-09-21 11:44:45 -0500

    Bueno ya

--- app/src/main/java/com/example/paycad/controller/ActivityIdentificationScreen.kt
+++ app/src/main/java/com/example/paycad/controller/ActivityIdentificationScreen.kt
@@ -1,12 +1,45 @@
 package com.example.paycad.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.paycad.R
 
 class ActivityIdentificationScreen : AppCompatActivity() {
+    private lateinit var inputNombre:EditText
+    private lateinit var inputMonto:EditText
+    private lateinit var btnNext:Button
+    companion object {
+        var nombre: String = ""
+        var monto: Int = 0
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_identification_screen)
+        initComponent()
+        initListeners()
+    }
+
+    private fun initComponent() {
+        btnNext = findViewById(R.id.btnNextIdentificaion)
+        inputNombre = findViewById(R.id.inputNombre)
+        inputMonto = findViewById(R.id.inuptMonto)
+    }
+
+    private fun initListeners() {
+        btnNext.setOnClickListener {
+            if (inputNombre.text.toString().trim().isEmpty() || inputMonto.text.toString().trim().isEmpty()){
+                Toast.makeText(this, "Ingrese todos los valores", Toast.LENGTH_SHORT).show()
+
+            }else{
+                nombre = inputNombre.text.toString()
+                monto = inputMonto.text.toString().toInt()
+                startActivity(Intent(this, ActivityVerificationScreen::class.java))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/paycad/controller/ActivityVerificationScreen.kt
+++ app/src/main/java/com/example/paycad/controller/ActivityVerificationScreen.kt
@@ -1,12 +1,46 @@
 package com.example.paycad.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.paycad.R
 
 class ActivityVerificationScreen : AppCompatActivity() {
+    private lateinit var nombre:TextView
+    private lateinit var codigo:EditText
+    private lateinit var btnGenerarNuevoCodigo:Button
+    private lateinit var btnNext:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification_screen)
+        initComponet()
+        initListeners()
+    }
+
+    private fun initComponet() {
+        nombre = findViewById(R.id.txtNombre)
+        codigo = findViewById(R.id.inputCodigo)
+        btnGenerarNuevoCodigo = findViewById(R.id.btnGenerarNuevocodigo)
+        btnNext = findViewById(R.id.btnNextVerificacion)
+
+        nombre.text = ActivityIdentificationScreen.nombre
+    }
+
+    private fun initListeners() {
+        btnNext.setOnClickListener{
+            if(codigo.text.toString().toInt() == ActivityGenerationCodeScreen.codigoObtenido){
+                val intent = Intent(this, ActivityHomeScreen::class.java)
+                startActivity(intent)
+            }else Toast.makeText(this, "El codigo es incorrecto", Toast.LENGTH_SHORT).show()
+        }
+
+        btnGenerarNuevoCodigo.setOnClickListener{
+            val intent = Intent(this, ActivityGenerationCodeScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
